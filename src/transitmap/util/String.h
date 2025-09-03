3ee7052c6e333cb025424fe80d962516bda85ab7
timbold <timbold87@gmail.com>
2025-09-03 23:49:25 +0800

    Refine station label sanitizer

--- src/transitmap/util/String.h
+++ src/transitmap/util/String.h
@@ -11,20 +11,13 @@ namespace util {
 inline std::string sanitizeStationLabel(const std::string& input) {
   std::wstring_convert<std::codecvt_utf8<wchar_t>> conv;
   std::wstring ws = conv.from_bytes(input);
+  std::locale loc("");
   std::wstring out;
-  bool prevUnderscore = false;
-  std::locale loc;
   for (wchar_t c : ws) {
-    if (std::isalnum(c, loc)) {
-      out.push_back(c);
-      prevUnderscore = false;
-    } else if (!prevUnderscore) {
-      out.push_back(L'_');
-      prevUnderscore = true;
+    if (std::iswalnum(c, loc)) {
+      out.push_back(std::towlower(c, loc));
     }
   }
-  while (!out.empty() && out.front() == L'_') out.erase(out.begin());
-  while (!out.empty() && out.back() == L'_') out.pop_back();
   return conv.to_bytes(out);
 }
 
