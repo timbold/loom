676dbec83fe16d6c047b0c1faf17fa192ca2ba31
Tim <temuulen@datadeus.mn>
2026-01-22 12:25:47 +0800

    Link util library and add minimal Test.h

---
+++ src/util/Test.h
@@ -0,0 +1,10 @@
+// Copyright 2017, University of Freiburg,
+// Chair of Algorithms and Data Structures.
+// Authors: Patrick Brosi <brosi@informatik.uni-freiburg.de>
+
+#ifndef UTIL_TEST_H_
+#define UTIL_TEST_H_
+
+#include "util/Misc.h"
+
+#endif  // UTIL_TEST_H_
